a0ba645e48ca008f68b29e5e39f9ae47dcb920ec
Xiangn <xiangnrodriguez@gmail.com>
2022-06-01 19:28:55 -0400

    Routes implementadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { MyTableComponent } from './my-table/my-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: MyDashboardComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabla',
+    component: MyTableComponent
+  },
+  {
+    path: '**',
+    component: MyDashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
